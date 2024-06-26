dfb58f4daa98cb08abf560205a144fd4e15575ab
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-06-25 17:41:22 -1000

    Add addtodoform component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
 
   return (
     <div>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
