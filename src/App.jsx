d86d3174a195b7e15e719e94ab64c56580fed2ea
Victoria Sevilla <126656971+v-sevilla@users.noreply.github.com>
2024-07-18 20:30:45 -1000

    Save todolist in storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
@@ -6,6 +6,11 @@ import AddTodoForm from './AddTodoForm';
 const App = () => {
 
   const [todoList, setTodoList] = useState([])
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", (todoList))
+  }, [todoList])
+
 
   const addTodo = (newTodo) => {
     setTodoList((previousTodoList) => [...previousTodoList, newTodo])
